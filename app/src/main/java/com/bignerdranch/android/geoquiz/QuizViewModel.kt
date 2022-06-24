4f536ce916593aafca21be478d5b558c8234d968
Hugo Camacho <hugofi@gmail.com>
2022-06-24 16:28:57 -0500

    GeoQuiz

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG="QuizViewModel"
+const val CURRENT_INDEX_KEY="CURRENT_INDEX_KEY"
 
 class QuizViewModel (private val savedStateHandle: SavedStateHandle) : ViewModel() {
     /*init {
